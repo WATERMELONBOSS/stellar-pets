e7d909510d4ff5631ab10ae68322fbd0f2a565f1
Abhishek Raju Palathoti <pabhishekraju110602@gmail.com>
2025-10-26 12:44:13 -0400

    Fix TypeScript errors for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { WalletProvider } from './contexts/WalletContext';
 import { PetProvider } from './contexts/PetContext';
